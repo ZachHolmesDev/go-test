bfc35ae641cb52b8c2ec6abbbaea7c2e6835c2c5
Zach Holmes <zholmes430@gmail.com>
2024-07-13 18:47:55 +1000

    work on test

--- main.go
+++ main.go
@@ -1,12 +1,45 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
-
-	"rsc.io/quote"
+	"log"
+	"os"
 )
 
+type App struct {
+    AppID int    `json:"appid"`
+    Name  string `json:"name"`
+}
+
+type AppList struct {
+    Apps []App `json:"apps"`
+}
+
+type AppListData struct {
+    AppList AppList `json:"applist"`
+}
+
 func main() {
-	// fmt.Print("Hello Go")
-	fmt.Print(quote.Go())
+	fmt.Print("Starting Go")
+	// fmt.Print(quote.Go())
+	json_data, err := os.ReadFile("./all_steam_games.json")
+	if err != nil {
+		log.Fatalf("err reading JSON file: %v")
+	}
+
+	var all_steam_games_data AppListData
+
+	err = json.Unmarshal(json_data, &all_steam_games_data)
+	if err != nil {
+		log.Fatalf("Error unmarshaling JSON: %v", err)
+	}
+
+	// outwave steam id
+	// game_id := "2050310"
+	game_name := "OutWave"
+
+	// call the function 
+
 }
